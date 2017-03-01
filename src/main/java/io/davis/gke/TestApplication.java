3b6f13a1284383e08f86be6a3fa89509a1ac8c76
Davis <davis.dai@go6d.com>
2017-03-01 14:31:57 +0800

    add travis script

---
+++ src/main/java/io/davis/gke/TestApplication.java
@@ -0,0 +1,14 @@
+package io.davis.gke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Davis on 17/3/1.
+ */
+@SpringBootApplication
+public class TestApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TestApplication.class, args);
+  }
+}
